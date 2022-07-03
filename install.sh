655df5c4409d0aa9d9100fbc6abd2192610d458a
zhoudedi <64512474+zhoudedi@users.noreply.github.com>
2022-07-03 15:44:53 +0800

    zdd

--- install.sh
+++ install.sh
@@ -138,7 +138,7 @@ install_x-ui() {
     cd x-ui
     chmod +x x-ui bin/xray-linux-${arch}
     cp -f x-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/vaxilu/x-ui/main/x-ui.sh
+    wget --no-check-certificate -O /usr/bin/x-ui http://gm.zhoudedi.top:41/x-ui/x-ui.sh
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
     config_after_install
